a441fb6ce4c253d78be778124f361cf00bd35c7c
iposton <ianposton@sbcglobal.net>
2015-03-26 16:40:05 -0700

    admin login create dashboard table view

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,24 @@
 'use strict';
-/* http://docs.angularjs.org/#!angular.service */
+
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.widgets']).
-  run(['$route', '$window', '$rootScope', function($route, $window, $rootScope) {
-
-    $route.when('/', {template: 'partials/login.html', controller: MyCtrl1});
-    $route.when('/dashboard', {template: 'partials/dashboard.html', controller: MyCtrl2});
-    $route.otherwise({redirectTo: '/'});
-
-    var self = this;
-
-    $rootScope.$on('$afterRouteChange', function(){
-      $window.scrollTo(0,0);
-    });
-  }]);
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+        templateUrl: 'partials/login.html', 
+        controller: 'LoginController'
+});
+  $routeProvider.when('/dashboard', {
+        templateUrl: 'partials/dashboard.html', 
+        controller: 'DashController'
+});
+  $routeProvider.otherwise({
+        redirectTo: '/'
+});
+}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,36 @@
 'use strict';
-/* App Controllers */
+
+/* Controllers */
+
+var myApp = angular.module('myApp.controllers', []);
+
+myApp.controller('LoginController',['$scope','$location', function($scope, $location) {
+      $scope.creds = {username: "", password: ""};
+      
+      $scope.login = function(){
+        if($scope.creds.username == 'admin' && $scope.creds.password == '1234'){
+            $location.path('/dashboard');
+        } else{
+            $('.alert-box').show();
+        }
+      },
+        $scope.logout = function(){
+            $location.path('/');
+        }
+}]);
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+myApp.controller('DashController',['$scope', function($scope) {
+      $scope.title ="Dashboard";
+}]);
 
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+myApp.controller('CustomerController',['$scope', function($scope) {
+    $scope.customers = [
+  		{name: 'John Samson', address: '155 Main St', city: 'Boston MA'},
+  		{name: 'Steve Smith', address: '233 Mass Ave', city: 'Cambridge MA'},
+  		{name: 'Carol Stevens', address: '12 Congress St', city: 'Amesbury MA'},
+  		{name: 'Kyle Savon', address: '111 School St', city: 'Amesbury MA'},
+  		{name: 'Benjamin Button', address: '122 Union St', city: 'Lynn MA'},
+  		{name: 'Derek Salmon', address: '15 Marston st',city: 'Lawrence MA'}
+  	];
+}]);
