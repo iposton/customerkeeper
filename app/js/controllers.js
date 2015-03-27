14f982315e1eb473ce818719415c0b589f3cf48e
iposton <ianposton@sbcglobal.net>
2015-03-26 18:02:04 -0700

    add search bar to customer list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,18 +4,14 @@
 
 var myApp = angular.module('myApp.controllers', []);
 
-myApp.controller('LoginController',['$scope','$location', function($scope, $location) {
-      $scope.creds = {username: "", password: ""};
+myApp.controller('LoginController',['$scope','AuthenticationService', function($scope, AuthenticationService) {
+        $scope.creds = {username: "", password: ""};
       
-      $scope.login = function(){
-        if($scope.creds.username == 'admin' && $scope.creds.password == '1234'){
-            $location.path('/dashboard');
-        } else{
-            $('.alert-box').show();
-        }
-      },
+        $scope.login = function(){
+            AuthenticationService.login($scope.creds);
+        },
         $scope.logout = function(){
-            $location.path('/');
+            AuthenticationService.logout();
         }
 }]);
 
@@ -24,13 +20,7 @@ myApp.controller('DashController',['$scope', function($scope) {
       $scope.title ="Dashboard";
 }]);
 
-myApp.controller('CustomerController',['$scope', function($scope) {
-    $scope.customers = [
-  		{name: 'John Samson', address: '155 Main St', city: 'Boston MA'},
-  		{name: 'Steve Smith', address: '233 Mass Ave', city: 'Cambridge MA'},
-  		{name: 'Carol Stevens', address: '12 Congress St', city: 'Amesbury MA'},
-  		{name: 'Kyle Savon', address: '111 School St', city: 'Amesbury MA'},
-  		{name: 'Benjamin Button', address: '122 Union St', city: 'Lynn MA'},
-  		{name: 'Derek Salmon', address: '15 Marston st',city: 'Lawrence MA'}
-  	];
+
+myApp.controller('CustomerController',['$scope', 'CustomerService', function($scope,CustomerService) {
+    $scope.customers = CustomerService.getCustomers();
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -1,7 +1,48 @@
 'use strict';
-/* http://docs.angularjs.org/#!angular.service */
+
+/* Services */
+
 
 // Demonstrate how to register services
-// In this case it is a simple constant service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+// In this case it is a simple value service.
+var myApp = angular.module('myApp.services', []);
+
+myApp.value('version', '0.1');
+
+myApp.factory("AuthenticationService",['$location', function($location){
+    return{
+        login: function(creds){
+            if(creds.username == 'admin' && creds.password == '1234'){
+                $location.path('/dashboard');
+            } else{
+                $('.alert-box').show();
+            }
+        },
+        logout: function(){
+            $location.path('/');
+        }
+    }
+}]);
+
+myApp.factory("CustomerService",[function(){
+    var customers = [
+  		{name: 'Chevy Chase', address: '222 Clark St', city: 'London United Kingdom'},
+  		{name: 'Steve Trinity', address: '111 Runyan Ave', city: 'Newton NJ'},
+  		{name: 'Cat Stevens', address: '33 Elm St', city: 'Fresno CA'},
+  		{name: 'Kim Korver', address: '1212 Fisher Ave', city: 'Salt Lake UT'},
+  		{name: 'Captain Dan ', address: '7 Forest Dr', city: 'New York NY'},
+  		{name: 'Wilson', address: '677 Rick St',city: 'Paris France'}
+  	];
+    
+    var factory = {};
+    
+    factory.getCustomers = function(){
+        return customers;
+    }
+    
+    factory.postCustomers = function(customer){
+        //code to add customer
+    }
+    
+    return factory;
+}]);
